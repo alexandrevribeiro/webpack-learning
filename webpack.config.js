3a803ed31a52bddd79aad10d45220c7243b1693f
alexandrevribeiro <vribeiro.alexandre@gmail.com>
2017-07-30 15:47:48 -0300

    Adding "babel-loader" to Webpack rules (loaders)

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,17 @@ const config = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
+    },
+    // Configuring the loaders (rules):
+    module: {
+        rules: [
+            {
+                use: 'babel-loader',
+                // "test" expects a Regular expression to be applied to the 
+                // filename of every file imported into the project.
+                test: /\.js$/ // -> verifies if the file ends with ".js"
+            }
+        ]
     }
 };
 
