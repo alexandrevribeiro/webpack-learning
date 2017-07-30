0989ec6662d68d0e432f6050f1c4949e46aec8eb
alexandrevribeiro <vribeiro.alexandre@gmail.com>
2017-07-30 17:02:54 -0300

    Using "extract-text-webpack-plugin" for css files (not working)

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const config = {
     entry: './src/index.js',
@@ -15,14 +16,24 @@ const config = {
                 // filename of every file imported into the project.
                 test: /\.js$/ // -> verifies if the file ends with ".js"
             },
+            // {
+            //     // The rules are applied from right to left, which means the "css-loader"
+            //     // will be applied first and then the "style-loader".
+            //     // - css-loader: Knows hot how to deal with "CSS imports"
+            //     // - style-loader: Takes "CSS imports" and adds them to the HTML document
+            //     use: ['style-loader', 'css-loader'],
+            //     test: /\.css$/
+            // }
             {
-                // The rules are applied from right to left, which means the "css-loader"
-                // will be applied first and then the "style-loader".
-                // - css-loader: Knows hot how to deal with "CSS imports"
-                // - style-loader: Takes "CSS imports" and adds them to the HTML document
-                use: ['style-loader', 'css-loader'],
-                test: /\.css$/
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
             }
+        ],
+        plugins: [
+            new ExtractTextPlugin("styles.css"),
         ]
     }
 };
