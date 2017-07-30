9c341bc2cfa83dacda456ffae7921301d211f107
alexandrevribeiro <vribeiro.alexandre@gmail.com>
2017-07-30 16:40:01 -0300

    Handling CSS with Webpack (being directly inject into "head" element form now)

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,14 @@ const config = {
                 // "test" expects a Regular expression to be applied to the 
                 // filename of every file imported into the project.
                 test: /\.js$/ // -> verifies if the file ends with ".js"
+            },
+            {
+                // The rules are applied from right to left, which means the "css-loader"
+                // will be applied first and then the "style-loader".
+                // - css-loader: Knows hot how to deal with "CSS imports"
+                // - style-loader: Takes "CSS imports" and adds them to the HTML document
+                use: ['style-loader', 'css-loader'],
+                test: /\.css$/
             }
         ]
     }
